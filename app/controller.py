b087eb0ef2f3c226331b333d112a6f1e7b6d8996
Mayank Kumar <mayank.kumar@hp.com>
2015-06-18 13:39:41 +0530

    Solve issue with GET all and GET Id specific operations

--- app/controller.py
+++ app/controller.py
@@ -4,15 +4,6 @@ from flask_restful import Resource, Api, abort
 
 api = Api(app)
 
-FAKE_DATA = [
-             {
-              'id': 1,
-              'name': 'mayank'},
-             {
-              'id': 2,
-              'name': 'vikram'}
-             ]
-
 def abort_if_res_non_existant(res_id):
     if res_id > len(FAKE_DATA):
         abort(404,
@@ -21,7 +12,7 @@ def abort_if_res_non_existant(res_id):
 
 class PinCodes(Resource):
     def get(self):
-        return postalinfo.query.all
+        return [{"pincode": x.pincode, "officename": x.officename} for x in postalinfo.query.filter().all()]
 
     def post(self):
         pass
@@ -30,10 +21,17 @@ class PinCodes(Resource):
 class PinCode(Resource):
     def get(self, res_id):
 #         abort_if_res_non_existant(int(res_id))
-        from flask import jsonify
-        val = jsonify(postalinfo.query.get(int(res_id)))
-        print val
-        return val
+        val = postalinfo.query.filter().get(res_id)
+        return {"Postal Index Number": res_id,
+                "Office Name": val.officename,
+                "Office Type": val.officeType,
+                "Delivery Status": val.Deliverystatus,
+                "Division Name": val.divisionname,
+                "Region Name": val.regionname,
+                "Circle Name": val.circlename,
+                "Taluk": val.Taluk,
+                "District Name": val.Districtname,
+                "State Name": val.statename}
 
     def delete(self, res_id):
         pass
